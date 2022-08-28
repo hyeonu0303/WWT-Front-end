0b65235abb6b209f6637155c173f9c319c302afc
scwon <a49297466@gmail.com>
2022-08-29 02:15:09 +0900

    feat: 실시간 20위 표시, 바로 검색

--- packages/presentation/pages/index.tsx
+++ packages/presentation/pages/index.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import type { NextPage } from 'next'
-import Head from 'next/head';
-import 'styles/style.css';
 import useTranslation from 'next-translate/useTranslation'
+import Container from '@mui/material/Container'
 
 const Home: NextPage = () => {
   const { t } = useTranslation()
@@ -11,39 +10,29 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     axios.get('http://bgung.iptime.org:7777/data').then((res: any) => {
-      const response = res.data.trend_rank;
-      const parsedData: Array<any> = [];
+      const response = res.data.trend_rank
+      const parsedData: Array<any> = []
       Object.keys(response).forEach((key) => {
         parsedData.push(response[key])
       })
       setData(parsedData)
     })
-  }, []);
+  }, [])
 
-  // useEffect(async () => {
-  //   try {
-  //     const response = await axios.get('https://worldtimeapi.org/api/ip');
-  //   }
-  //   catch (e) {
-  //     console.error(e);
-  //   }
-  // },[]);
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>WWT</title>
-        <meta name="description" content="World-Wide-Trends" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>{t('common:title')}</h1>
-      {data.map((keyword: string, i) => {
-        return (
-          <h1 key={keyword}>
-            {i + 1}위: {keyword}
-          </h1>
-        )
-      })}
-      <footer className={styles.footer}></footer>
+    <div>
+			<Container>
+				<h1 style={{ textAlign: 'center' }}>World Wide Trends</h1>
+				{/* <h1>{t('common:title')}</h1> */}
+				{data.map((keyword: string, i) => {
+					return (
+						<h1 key={keyword}>
+							{i + 1}위: <a target="_blank" rel='noreferrer' href={`https://www.google.com/search?q=${keyword}`}>{keyword}</a>
+						</h1>
+					)
+				})}
+			</Container>
+      <footer></footer>
     </div>
   )
 }
