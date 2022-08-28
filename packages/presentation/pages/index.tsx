c309efbed7fcc9f565324300c34a79ca91689554
scwon <a49297466@gmail.com>
2022-08-28 20:27:31 +0900

    build: Monorepo 환경 구성

--- packages/presentation/pages/index.tsx
+++ packages/presentation/pages/index.tsx
@@ -7,19 +7,19 @@ import styles from '../styles/Home.module.css'
 import useTranslation from 'next-translate/useTranslation'
 
 const Home: NextPage = () => {
-  const { t } = useTranslation();
-	const [data, setData] = useState<string[]>([])
+  const { t } = useTranslation()
+  const [data, setData] = useState<string[]>([])
 
   useEffect(() => {
     axios.get('http://192.168.0.68:8000/data').then((res: any) => {
-			const response = res.data;
-			console.log("response =", response);
-			const parsedData: Array<any> = [];
-			Object.keys(response).forEach(key => {
-				parsedData.push(response[key]);
-			});
-			setData(parsedData);
-		})
+      const response = res.data
+      console.log('response =', response)
+      const parsedData: Array<any> = []
+      Object.keys(response).forEach((key) => {
+        parsedData.push(response[key])
+      })
+      setData(parsedData)
+    })
   }, [])
 
   // useEffect(async () => {
@@ -38,13 +38,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>{t('common:title')}</h1>
-			{
-				data.map((keyword: string, i) => {
-					return (
-						<h1 key={keyword}>{i + 1}위: {keyword}</h1>
-					);
-				})
-			}
+      {data.map((keyword: string, i) => {
+        return (
+          <h1 key={keyword}>
+            {i + 1}위: {keyword}
+          </h1>
+        )
+      })}
       <footer className={styles.footer}></footer>
     </div>
   )
