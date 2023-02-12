99094042c2cc8f64ec888729adcc3874e8438aa7
scwon <a49297466@gmail.com>
2023-02-12 22:09:37 +0900

    add header

---
+++ src/lib/layouts/NavbarLayout.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Logo from '$lib/assets/image/logo.png';
+</script>
+
+<div class="wrapper">
+	<nav>
+		<a href="/">
+			<img class="logo" src={Logo} alt="Horizon Logo Image" />
+		</a>
+	</nav>
+	<div class="contents">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.contents {
+		flex: 1;
+		width: 100%;
+		overflow: auto;
+	}
+	.wrapper {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+	nav {
+		height: 70px;
+		border-bottom: 1px solid black;
+		background: white;
+		text-align: center;
+	}
+	.logo {
+		height: 70px;
+		padding: 10px 0px;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import 'carbon-components-svelte/css/white.css';
+	import './styles.css';
+	import NavbarLayout from '$lib/layouts/NavbarLayout.svelte';
+</script>
+
+<NavbarLayout>
+	<slot />
+</NavbarLayout>
