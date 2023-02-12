beb06e94f9da806a18c6de979c8576b0b6d8b51c
scwon <a49297466@gmail.com>
2023-02-12 23:16:34 +0900

    시계 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+	import DigitalClock from '$lib/components/DigitalClock.svelte';
+</script>
+
+<div>
+	<div class="clockArea"><DigitalClock /></div>
+</div>
+
 <style>
+	.clockArea {
+		height: 300px;
+	}
 </style>
