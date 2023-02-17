626ece3e4fe212ad3001017883ed2f1380eff26f
scwon <a49297466@gmail.com>
2023-02-17 23:43:33 +0900

    고양이는 귀여워

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,65 @@
 <script>
-	import DigitalClock from '$lib/components/DigitalClock.svelte';
-	import { DatePicker, DatePickerInput } from 'carbon-components-svelte';
+	import Search from '$lib/assets/image/search.png';
 </script>
 
-<div>
-	<!-- <div class="clockArea">
-		<DigitalClock />
-		<div class="datePickerArea">
-			<DatePicker datePickerType="single" on:change>
-				<DatePickerInput labelText="Meeting date" placeholder="mm/dd/yyyy" />
-			</DatePicker>
+<div class="wrapper">
+	<div class="cover">
+		<div class="searchBox">
+			<input class="search" />
+			<div class="searchButton">
+				<img width="32" src={Search} alt="search" />
+			</div>
 		</div>
-	</div> -->
+	</div>
 </div>
 
 <style>
-	.clockArea {
-		height: 300px;
+	.wrapper {
+		width: 100%;
+		height: 100%;
+		background-image: url('$lib/assets/image/cat.jpg');
+		background-size: cover;
 		position: relative;
 	}
-	.datePickerArea {
+	.cover {
 		position: absolute;
-		right: 10px;
-		bottom: 10px;
+		width: 100%;
+		height: 100%;
+		left: 0;
+		top: 0;
+		background: linear-gradient(45deg, rgba(234, 56, 98, 0.6), rgba(249, 221, 198, 0.6));
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.searchBox {
+		width: 1000px;
+		max-width: 80%;
+		height: 80px;
+		/* background-color: rgb(234, 56, 98); */
+		border: 8px solid rgb(234, 56, 98);
+		display: flex;
+	}
+
+	.search {
+		flex: 1;
+		height: 100%;
+		border: none;
+		outline: none;
+		font-size: 24px;
+		padding: 8px;
+		color: white;
+		font-weight: bold;
+		text-align: center;
+		background: rgba(234, 56, 98, 0.2);
+	}
+
+	.searchButton {
+		width: 64px;
+		background-color: rgb(234, 56, 98);
+		cursor: pointer;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 </style>
