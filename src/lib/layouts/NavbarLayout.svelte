0956ff95a5fa5f42f0112084191621f8fc3d01ed
scwon <a49297466@gmail.com>
2023-03-26 17:29:53 +0900

    feat: 프로젝트 변경

--- src/lib/layouts/NavbarLayout.svelte
+++ src/lib/layouts/NavbarLayout.svelte
@@ -1,63 +1,10 @@
 <script>
-	import Logo from '$lib/assets/image/logo.png';
+	// import Logo from '$lib/assets/image/logo.png';
 </script>
 
 <div class="wrapper">
-	<nav>
-		<a class="logoArea" href="/">
-			<img class="logo" alt="Horizon Logo" src={Logo} />
-		</a>
-	</nav>
+	<nav />
 	<div class="contents">
 		<slot />
 	</div>
 </div>
-<!-- 234.56.98 -->
-
-<!-- 249.221.198 -->
-<style>
-	.contents {
-		flex: 1;
-		width: 100%;
-		overflow: auto;
-	}
-	.wrapper {
-		height: 100%;
-		display: flex;
-		flex-direction: column;
-	}
-	nav {
-		height: 70px;
-		background: linear-gradient(45deg, rgb(234, 56, 98), rgb(249, 221, 198));
-	}
-	.logoArea {
-		background-color: white;
-		display: block;
-		width: 210px;
-		padding: 0px 20px;
-		transform: skew(-20deg);
-		height: 100%;
-		transition: 0.2s;
-		position: relative;
-	}
-
-	.logoArea::after {
-		left: 0px;
-		position: absolute;
-		top: 0px;
-		height: 100%;
-		width: 20px;
-		background-color: white;
-		content: '';
-		transform: skew(20deg);
-	}
-
-	.logoArea:hover {
-		width: 250px;
-	}
-	.logo {
-		height: 70px;
-		padding: 10px 0px;
-		transform: skew(20deg);
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,92 +1,10 @@
 <script>
 	import Search from '$lib/assets/image/search.png';
+	let text = '';
+
+	const onSearch = () => {
+		window.open(`/search?query=${text}`, '_self');
+	};
 </script>
 
-<div class="wrapper">
-	<div class="cover">
-		<h1>WORLD<br /> WIDE<br />TRENDS</h1>
-		<div class="searchBox">
-			<div class="inputWrapper">
-				<input class="search" />
-			</div>
-			<div class="searchButton">
-				<img width="32" src={Search} alt="search" />
-			</div>
-		</div>
-	</div>
-</div>
-
-<style>
-	.wrapper {
-		width: 100%;
-		height: 100%;
-		background-image: url('$lib/assets/image/cat.jpg');
-		background-size: cover;
-		position: relative;
-	}
-	.cover {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		left: 0;
-		top: 0;
-		background: linear-gradient(45deg, rgba(234, 56, 98, 0.6), rgba(249, 221, 198, 0.6));
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-	.searchBox {
-		width: 1000px;
-		max-width: 50%;
-		height: 80px;
-		/* background-color: rgb(234, 56, 98); */
-		border: 8px solid rgb(234, 56, 98);
-		display: flex;
-		border-radius: 8px;
-	}
-
-	.inputWrapper {
-		flex: 1;
-	}
-	.search {
-		width: 100%;
-		height: 100%;
-		border: none;
-		outline: none;
-		font-size: 24px;
-		padding: 8px;
-		color: white;
-		font-weight: bold;
-		text-align: center;
-		background: rgba(234, 56, 98, 0.2);
-	}
-
-	.searchButton {
-		width: 64px;
-		background-color: rgb(234, 56, 98);
-		cursor: pointer;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-
-	h1 {
-		font-size: 80px;
-		font-weight: bold;
-		color: white;
-		position: absolute;
-		left: 16px;
-		line-height: 70px;
-	}
-
-	@media screen and (max-width: 991px) {
-		h1 {
-			top: 36px;
-			left: 10%;
-		}
-
-		.searchBox {
-			max-width: 80%;
-		}
-	}
-</style>
+<h1>MaFor</h1>
